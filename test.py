9ead886bb59d6ee1f1eb654ebd399f89d2f8ae40
coderpower0 <coderpower0@gmail.com>
2019-05-21 17:47:39 +0800

    adding new function

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+from function import some_fun
+
 array = []
 user_input = input("separate numbers with comma: ").strip()
 
 array = [int(item) for item in user_input.split(',')]
 
-print(array)
+print(array, some_fun())
